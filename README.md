ffcb1b718920081b41e48b4a77e6837f4bb4d3ea
whitehatprojects <rahulgarg6662@gmail.com>
2020-10-02 14:42:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Project-Sixty-Eight
---
+++ screens/facebook.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Facebook extends React.Component {
+  render(){
+    return (
+      <View style={{flex: 1,backgroundColor: '#fff',alignItems: 'center',justifyContent: 'center',}}>
+        <Text>Facebook</Text>
+      </View>
+    );
+  }
+}
